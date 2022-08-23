a682dc139e4d3545ce57cac6d515ea09c6730f88
KHR605 <sbafmfhs123@naver.com>
2022-08-23 22:10:36 +0900

    del

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Main(){
+    return(
+        <header id="header">
+            <h1>AmericaTV</h1>
+            <div className='util'></div>
+            <div className='searchArea'></div>
+        </header>
+        
+    );
+}
+
+export default Main;
