f3eb256fb5b61adcf9c2cc65a70b5ac37f49ab16
KHR605 <sbafmfhs123@naver.com>
2022-09-20 20:48:51 +0900

    hotfix - loginjs , proxy, BE_CORS

--- backend/src/main/java/com/americatv/config/CorsConfig.java
+++ backend/src/main/java/com/americatv/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
       config.addAllowedOrigin("http://localhost:3000");
-      config.addAllowedOrigin("*");
+      config.addAllowedOriginPattern("*");
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,10 +2,13 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
     app.use(
-        '/api',
+        '/ayj',
         createProxyMiddleware({
             target: 'http://localhost:8080',
             changeOrigin: true,
         })
     );
 }
+
+
+//1. setupProxy 주석 -> npm start -> 연결이 안됨 -> 주석 해제 -> 로그인해서 값 확인 -> 504err -> 서버를 켰어 -> 됐음
