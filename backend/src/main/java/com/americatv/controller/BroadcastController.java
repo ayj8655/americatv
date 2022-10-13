55ca71c3c6f37b0970cce648068b156bcb32c6dc
ayj8655 <ayj8655@naver.com>
2022-10-13 23:32:51 +0900

    방송국 정보 읽기, 유저 정보 수정

--- backend/src/main/java/com/americatv/controller/BroadcastController.java
+++ backend/src/main/java/com/americatv/controller/BroadcastController.java
@@ -3,13 +3,19 @@ package com.americatv.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.americatv.entity.BlackList;
+import com.americatv.entity.Broadcast;
+import com.americatv.entity.User;
 import com.americatv.service.BlackListService;
+import com.americatv.service.BroadcastService;
+import com.americatv.service.UserService;
 
 
 @RestController
@@ -18,6 +24,12 @@ public class BroadcastController {
 	
 	@Autowired
 	public BlackListService blackListService;
+	
+	@Autowired
+	public BroadcastService broadcastService;
+	
+	@Autowired
+	public UserService userService;
 	
 	@GetMapping("/")
 	public String index() {
@@ -32,6 +44,17 @@ public class BroadcastController {
 		System.out.println(blackList);
 		return blackList.toString();
 	}
+	
+	
+	
+	@GetMapping("/{userId}")
+	public ResponseEntity<Optional<Broadcast>> GetBroadcastInfo(@PathVariable String userId){
+//		System.out.println(userId);
+		Optional<User> user = userService.findeByUserId(userId);
+		return ResponseEntity.ok(broadcastService.findeByUserCd(user.get().getUserCd()));
+		
+	}
+	
 	
 	
 }
